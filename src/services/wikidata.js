43286f6dc6451247c1b28adf54071435f3b69153
Paweł Marynowski <pawel.marynowski@outlook.com>
2017-10-20 20:01:02 +0200

    feat(services): add wikidata service

---
+++ src/services/wikidata.js
@@ -0,0 +1,15 @@
+import Axios from 'axios';
+
+export default class wikidata {
+
+  constructor() {
+    this.wikidataUrl = 'https://www.wikidata.org/w/api.php';
+    this.sparqlUrl = 'https://query.wikidata.org/sparql';
+  }
+
+  runQuery(query) {
+    return Axios.get(this.sparqlUrl, {
+      params: { query },
+    });
+  }
+}
