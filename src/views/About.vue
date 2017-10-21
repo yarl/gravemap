1160a9c94e8c02861b75f62e1e3aa8ffdbd91924
Paweł Marynowski <yarl@o2.pl>
2017-10-22 00:22:53 +0200

    feat(About): add test content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,18 @@
 <template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Section</h1>
+      <h2 class="subtitle">
+        A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+        <a href="http://github.com/yarl/gravemap">github.com/yarl/gravemap</a>
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -11,4 +25,7 @@ export default {
 </script>
 
 <style scoped>
+  p {
+    margin-bottom: .5rem;
+  }
 </style>
